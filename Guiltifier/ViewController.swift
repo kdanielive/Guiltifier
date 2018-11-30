0956b11b6027efe9dd27f4e76a0772fe196d8037
Daniel Kim <danielkim@dyn-160-39-236-208.dyn.columbia.edu>
2018-11-29 23:38:54 -0500

    Added storyboard components for the most basics

--- Guiltifier/ViewController.swift
+++ Guiltifier/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var chargeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        chargeButton.layer.cornerRadius = 5
+        chargeButton.layer.borderWidth = 0.5
+        chargeButton.layer.borderColor = UIColor.blue.cgColor
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
