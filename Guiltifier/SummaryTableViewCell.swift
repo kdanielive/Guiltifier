fcbef50e891d167c270fd4842d6f31a2e05ed5e8
Daniel Kim <danielkim@dyn-160-39-236-185.dyn.columbia.edu>
2018-12-02 21:25:29 -0500

    New plans

---
+++ Guiltifier/SummaryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SummaryTableViewCell.swift
+//  Guiltifier
+//
+//  Created by Daniel Kim on 12/2/18.
+//  Copyright © 2018 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class SummaryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
