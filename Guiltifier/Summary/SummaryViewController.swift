f4023c69c24b8ba4c17fb4fa6fb5c03dd2db9525
Daniel Kim <danielkim@dyn-160-39-236-24.dyn.columbia.edu>
2018-12-04 00:11:49 -0500

    One more data for entry data?

--- Guiltifier/Summary/SummaryViewController.swift
+++ Guiltifier/Summary/SummaryViewController.swift
@@ -12,6 +12,7 @@ import CoreData
 class SummaryViewController: UIViewController {
 
     @IBOutlet var endLineView: UIView!
+    @IBOutlet var endLineLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +23,33 @@ class SummaryViewController: UIViewController {
         entriesFetch.sortDescriptors = [NSSortDescriptor.init(key: "time", ascending: true)]
         let entries = try! managedContext.fetch(entriesFetch) as! [Entry]
         
+        var thisMonthPot = 0
+        
         for entry in entries {
             let rawTime = entry.time!
+            print(rawTime)
             var stringIndex = rawTime.index(rawTime.startIndex, offsetBy: 8)
             var date = String(rawTime[stringIndex...])
-            stringIndex = date.index(date.startIndex, offsetBy: 10)
+            stringIndex = date.index(date.startIndex, offsetBy: 7)
             date = String(date[...stringIndex])
             print("Date: ", date)
+            
+            let currentTime = Date().description(with: .current)
+            print(currentTime)
+            stringIndex = currentTime.index(currentTime.startIndex, offsetBy: 8)
+            var currentDate = String(currentTime[stringIndex...])
+            stringIndex = currentDate.index(currentDate.startIndex, offsetBy: 7)
+            currentDate = String(currentDate[...stringIndex])
+            print("Current Date: ", currentDate)
+            
+            if(date == currentDate) {
+                print("yes")
+                thisMonthPot += Int(entry.price!)!
+            }
+
         }
+        
+        endLineLabel.text = String(thisMonthPot)
         
         
         // Do any additional setup after loading the view.
